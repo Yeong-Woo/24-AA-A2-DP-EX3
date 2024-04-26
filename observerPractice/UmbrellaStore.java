604f76ccdba5b5af33a20bec0d99f86f2eff8241
yw0.kim <dn9078@naver.com>
2024-04-26 14:01:56 +0900

    Format

--- observerPractice/UmbrellaStore.java
+++ observerPractice/UmbrellaStore.java
@@ -1,23 +1,24 @@
 package observerPractice;
 
 public class UmbrellaStore {
-	
+
 	private boolean sellingState;
+
 	// default constructor
 	public UmbrellaStore() {
 		this.sellingState = false;
 	}
-	
+
 	public void whetherChanged(float rainfall) {
 		if (rainfall >= 5.0f)// when rainfall exceeds 5.0mm
 			sellingState = true;
 		else // when rainfall < 5.0
 			sellingState = false;
-		
+
 		isSelling();
 	}
 
 	public void isSelling() {
-		System.out.println("Umbrella "+ ((sellingState) ? "":"Not ") + "Sale !");
+		System.out.println("Umbrella " + ((sellingState) ? "" : "Not ") + "Sale !");
 	}
 }
